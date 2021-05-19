a125326483063c601d86719161de867774f9f82d
Ayusummer <1369661643@qq.com>
2021-05-19 21:51:55 +0800

    不定期存储

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-05-15 09:58:28
+ * @LastEditTime: 2021-05-15 09:58:29
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \class-management-assistance-bot\CHANGELOG.md
+-->
--- bot.py
+++ bot.py
@@ -1,14 +1,19 @@
 '''
 Author: your name
 Date: 2021-03-02 00:05:07
-LastEditTime: 2021-03-02 00:05:09
+LastEditTime: 2021-05-15 09:31:48
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: \nonebot-test\bot.py
 '''
-import nonebot
 
-if __name__ == '__main__':
-    nonebot.init()
-    nonebot.load_builtin_plugins()
+import nonebot
+from nonebot.adapters.cqhttp import Bot as CQHTTPBot
+
+nonebot.init()                                  # 使用默认配置初始化 NoneBot
+driver = nonebot.get_driver()
+driver.register_adapter("cqhttp", CQHTTPBot)    # 加载 NoneBot 内置的 CQHTTP 协议适配组件
+nonebot.load_builtin_plugins()                  # 加载 NoneBot 内置的插件
+
+if __name__ == "__main__":
     nonebot.run(host='127.0.0.1', port=8083)
